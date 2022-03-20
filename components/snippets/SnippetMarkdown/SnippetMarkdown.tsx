0315ca42ac44ca1f94569e684be5408e98a28d8f
Serena Antonetti <45170586+serenastorm@users.noreply.github.com>
2022-03-20 18:33:53 +0000

    [styling] make sure code tabs are rendered correctly

--- components/snippets/SnippetMarkdown/SnippetMarkdown.tsx
+++ components/snippets/SnippetMarkdown/SnippetMarkdown.tsx
@@ -14,83 +14,81 @@ import blogArticleStyles from "styles/blog/BlogArticle.module.scss";
 const SnippetMarkdown = ({ content }: { content: CoreOptions["children"] }) => {
   return (
     <>
-      <div>
-        <ReactMarkdown
-          // eslint-disable-next-line react/no-children-prop
-          children={content}
-          key="main"
-          className={styles.markdown}
-          remarkPlugins={[remarkGfm]}
-          rehypePlugins={[rehypeMetaAsAttributes, rehypeHighlight, rehypeRaw]}
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || "");
-              const filename = node?.properties?.filename;
+      <ReactMarkdown
+        // eslint-disable-next-line react/no-children-prop
+        children={content}
+        key="main"
+        className={`${styles.markdown} markdown`}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeMetaAsAttributes, rehypeHighlight, rehypeRaw]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            const filename = node?.properties?.filename;
 
-              const renderSnippet = () => (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              );
+            const renderSnippet = () => (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
 
-              return inline ? (
-                renderSnippet()
-              ) : (
-                <>
-                  {filename && (
-                    <div className={styles.codeBlockNav}>
-                      <div>
-                        <code>{filename}</code>
-                      </div>
+            return inline ? (
+              renderSnippet()
+            ) : (
+              <>
+                {filename && (
+                  <div className={styles.codeBlockNav}>
+                    <div>
+                      <code>{filename}</code>
                     </div>
-                  )}
-                  <pre>
-                    <SnippetCopyToClipboardButton
-                      textToCopy={reactNodeToString(children)}
-                    />
-                    {/* {match && (
+                  </div>
+                )}
+                <pre>
+                  <SnippetCopyToClipboardButton
+                    textToCopy={reactNodeToString(children)}
+                  />
+                  {/* {match && (
                           <SnippetPill
                             type={match[1]}
                             className={blogArticleStyles.codeSnippetPill}
                           />
                         )} */}
-                    {renderSnippet()}
-                  </pre>
-                </>
-              );
-            },
-            pre({ children }) {
-              return <>{children}</>;
-            },
-            a({ children, node, href, ...props }) {
-              const indexOfExternalLink = node.children.findIndex(
-                (child) =>
-                  child.type === "text" &&
-                  (child.value === "View full criteria" ||
-                    child.value === "View design pattern")
-              );
+                  {renderSnippet()}
+                </pre>
+              </>
+            );
+          },
+          pre({ children }) {
+            return <>{children}</>;
+          },
+          a({ children, node, href, ...props }) {
+            const indexOfExternalLink = node.children.findIndex(
+              (child) =>
+                child.type === "text" &&
+                (child.value === "View full criteria" ||
+                  child.value === "View design pattern")
+            );
 
-              const isExternalLink = indexOfExternalLink > -1;
+            const isExternalLink = indexOfExternalLink > -1;
 
-              const nodeChildren: Array<ElementContent & { value?: string }> =
-                node.children;
-              const linkCopy = nodeChildren[indexOfExternalLink]?.value;
+            const nodeChildren: Array<ElementContent & { value?: string }> =
+              node.children;
+            const linkCopy = nodeChildren[indexOfExternalLink]?.value;
 
-              return isExternalLink && href && !!linkCopy ? (
-                <NewTabLink
-                  copy={linkCopy}
-                  to={href}
-                  shouldOpenInNewTab
-                  className={`medium ${blogArticleStyles.newTabLink}`}
-                  withUnderline={false}
-                />
-              ) : (
-                <a {...props}>{children}</a>
-              );
-            },
-          }}
-        />
-      </div>
+            return isExternalLink && href && !!linkCopy ? (
+              <NewTabLink
+                copy={linkCopy}
+                to={href}
+                shouldOpenInNewTab
+                className={`medium ${blogArticleStyles.newTabLink}`}
+                withUnderline={false}
+              />
+            ) : (
+              <a {...props}>{children}</a>
+            );
+          },
+        }}
+      />
     </>
   );
 };
