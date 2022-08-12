938328161b2aa3fa54b01ac5efe05a8a0baa6548
serenastorm <serena.antonetti@live.fr>
2022-08-12 11:25:40 +0100

    creating changelog page based on accessibility page

--- infrastructure/routes/constants.ts
+++ infrastructure/routes/constants.ts
@@ -1,61 +1,19 @@
-import { SectionName, SectionTitle } from "./types";
-
 const api = {
   dev: "http://localhost:5000/api",
   prod: "https://antonettiserena-api.herokuapp.com/api",
 };
 
 export const apiUrl =
-  process.env.NODE_ENV === "production" ? api.prod : api.dev;
+  process.env.NODE_ENV === "production" ? api.prod : api.prod;
 
 const baseUrl = "/";
-const caseStudyBaseUrl = `${baseUrl}myqr`;
-const caseStudySectionUrl = (section?: SectionName) =>
-  `${caseStudyBaseUrl}/${section}`;
 const codeSnippetsBaseUrl = `${baseUrl}snippets`;
 const codeSnippetUrl = (slug: string) => `${codeSnippetsBaseUrl}/${slug}`;
 
-export const caseStudySectionsTitles = {
-  marketing: "Marketing" as SectionTitle,
-  dashboard: "User flows" as SectionTitle,
-  accessibility: "Accessibility" as SectionTitle,
-  features: "Features" as SectionTitle,
-  designSystem: "Design System" as SectionTitle,
-};
-
-const caseStudySections = {
-  marketing: "marketing" as SectionName,
-  dashboard: "user-flows" as SectionName,
-  accessibility: "accessibility" as SectionName,
-  features: "features" as SectionName,
-  designSystem: "design-system" as SectionName,
-};
-
 export const routes = {
   home: baseUrl,
-  myqr: {
-    intro: { title: "Intro", url: caseStudyBaseUrl },
-    designSystem: {
-      title: caseStudySectionsTitles.designSystem,
-      url: caseStudySectionUrl(caseStudySections.designSystem),
-    },
-    marketing: {
-      title: caseStudySectionsTitles.marketing,
-      url: caseStudySectionUrl(caseStudySections.marketing),
-    },
-    dashboard: {
-      title: caseStudySectionsTitles.dashboard,
-      url: caseStudySectionUrl(caseStudySections.dashboard),
-    },
-    accessibility: {
-      title: caseStudySectionsTitles.accessibility,
-      url: caseStudySectionUrl(caseStudySections.accessibility),
-    },
-    features: {
-      title: caseStudySectionsTitles.features,
-      url: caseStudySectionUrl(caseStudySections.features),
-    },
-  },
+  accessibility: `${baseUrl}accessibility`,
+  changelog: `${baseUrl}changelog`,
   blog: {
     snippet: {
       title: "Snippet",
---
+++ pages/changelog.tsx
@@ -0,0 +1,34 @@
+import { Page } from "components/shared/Page";
+
+import { NewTabLink } from "components/shared";
+
+import styles from "styles/Accessibility.module.scss";
+import blogStyles from "styles/blog/Blog.module.scss";
+import blogPageStyles from "styles/blog/BlogPage.module.scss";
+
+const Changelog = () => {
+  return (
+    <>
+      <Page
+        className={`${blogStyles.blog} ${blogPageStyles.blogPage} ${styles.accessibilityPage}`}
+      >
+        <h1 className={blogStyles.blogIndexTitle}>Changelog</h1>
+        <div className={blogStyles.blogPost}>
+          <p>
+            The web should be accessible to everyone. If your accessibility
+            requirements are not met, please{" "}
+            <NewTabLink
+              copy="open an issue on Github."
+              to="https://github.com/serenastorm/nextjs-portfolio/issues"
+              shouldOpenInNewTab
+              className={`medium ${blogStyles.newTabLink}`}
+              withUnderline={false}
+            />
+          </p>
+        </div>
+      </Page>
+    </>
+  );
+};
+
+export default Changelog;
