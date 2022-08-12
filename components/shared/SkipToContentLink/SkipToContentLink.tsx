19dbf8eb2d680bc55e7b1c6a94f7d645f71fa2a3
serenastorm <serena.antonetti@live.fr>
2022-08-12 15:06:04 +0100

    improve navigation focus styles

--- components/shared/SkipToContentLink/SkipToContentLink.tsx
+++ components/shared/SkipToContentLink/SkipToContentLink.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 import styles from "./SkipToContentLink.module.scss";
@@ -7,17 +6,9 @@ const SkipToContentLink = () => {
   const { pathname } = useRouter();
 
   const isSnippets = pathname.startsWith("/snippets/");
-  const isAccessibility = pathname.startsWith("/accessibility");
 
   return (
     <>
-      {!isAccessibility && (
-        <Link href="/accessibility" passHref>
-          <a className={styles.skipToContentLink}>
-            Accessibility feedback
-          </a>
-        </Link>
-      )}
       {isSnippets && (
         <a className={styles.skipToContentLink} href="#mainContent">
           Skip to main content
