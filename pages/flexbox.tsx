59d4bb8d226bc060abe108ec9604ac9b5686a2df
serenastorm <serena.antonetti@live.fr>
2022-10-10 00:26:18 +0200

    update flexbox thumbnail and font import

--- pages/flexbox.tsx
+++ pages/flexbox.tsx
@@ -8,6 +8,14 @@ const FlexboxPage = () => {
     <>
       <Head>
         <title>Flexbox | Serena Antonetti</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,500;1,400&display=swap"
+          rel="stylesheet"
+        />
         <meta name="theme-color" content="#181a1d" />
       </Head>
       <div className={styles.lessonPage}>
